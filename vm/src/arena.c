839e2fa0dadba65d74ca1adf0193bc07a5d4e6fd
Somaliika <iren_33@bigmir.net>
2019-09-12 19:29:36 +0300

    Get magic number

--- vm/src/arena.c
+++ vm/src/arena.c
@@ -9,7 +9,7 @@ void    set_players_to_arena(t_champ *champ, char **arena)
     quantity = get_quantity_players(champ);
     while(champ)
     {
-        ft_memcpy(&arena[0][MEM_SIZE / quantity * i], champ->code, ft_strlen(champ->code));
+        ft_memcpy(&arena[0][MEM_SIZE / quantity * i], champ->byte_code->code, champ->byte_code->length);
         champ = champ->next_champ;
         i++;
     }
