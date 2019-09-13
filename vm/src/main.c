44a2ec8cdae6dd311f4b7c20f3bc0b9979ed6afd
Somaliika <iren_33@bigmir.net>
2019-09-13 15:24:05 +0300

    Introducing players in vm

--- vm/src/main.c
+++ vm/src/main.c
@@ -12,6 +12,17 @@
 
 #include "../../includes/corewar.h"
 
+void	introducing(t_champ *ch)
+{
+	ft_printf("Introducing contestants...\n");
+	while (ch)
+	{
+		ft_printf("* Player %i, weighing %i bytes, \"%s\" (\"%s\") !\n",
+			ch->id, ch->byte_code->length, ch->header->prog_name, ch->header->comment);
+		ch = ch->next_champ;
+	}
+	
+}
 int     main(int argc, char **argv)
 {
 	t_vm	*vm;
@@ -21,5 +32,6 @@ int     main(int argc, char **argv)
 	create_arena(vm);
 	create_carriages(vm);
 	// system("leaks -q corewar");
+	introducing(vm->champs);
     return (0);
 }
--- vm/src/utils.c
+++ vm/src/utils.c
@@ -25,24 +25,6 @@ int get_quantity_players(t_champ *champ)
     return (size);
 }
 
-void	*ft_memrev(const void *src, size_t n)
-{
-	size_t	i;
-	char	*s1;
-	char	*s2;
-
-	i = 0;
-	s1 = (char *)ft_memalloc(n);
-	s2 = (char *)src;
-	while (n)
-	{
-		s1[i] = s2[n];
-		i++;
-		n--;
-	}
-	return (s1);
-}
-
 int	byte_to_int(char *str)
 {
 	char	*tmp;
