842f8a926d84166490077337129965600448e38f
dlian <dlian@mail.com>
2019-09-25 19:17:49 +0300

    ok

--- assembler/src/code_handle.c
+++ assembler/src/code_handle.c
@@ -7,8 +7,15 @@ t_list  *get_code_by_mark(t_list  *code_list, char *mark)
     while (code_list)
     {
         code = (t_code *)(code_list->content);
-        if (code->mark && ft_strequ(mark, code->mark->content))
-            return (code_list);
+        if (code->mark)
+        {
+            while (code->mark)
+            {
+                if (ft_strequ(code->mark->content, mark))
+                    return (code_list);
+                code->mark = code->mark->next;
+            }
+        }
         code_list = code_list->next;
     }
     return (NULL);
